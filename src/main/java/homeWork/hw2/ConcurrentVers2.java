9ebd4de3f21a8a91bcc627778cd52e4ace8df6c8
Pavel Korobko <110892851+kpd1989@users.noreply.github.com>
2022-08-18 19:42:40 +0600

    create versio2 home work

---
+++ src/main/java/homeWork/hw2/ConcurrentVers2.java
@@ -0,0 +1,50 @@
+package homeWork.hw2;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class ConcurrentVers2 {
+    private static String [] sqrtList = new String[1000000];
+    private static final int numberTread = 10; // количество потоков
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        long startTime = System.currentTimeMillis();
+        BufferedWriter writerToFile = new BufferedWriter(new FileWriter("sqrtList.csv"));
+        ArrayList<Thread> threads = new ArrayList<>();
+
+
+        for (int threadNumber = 0; threadNumber < numberTread; ++threadNumber) {
+            int incrementWorkRange = (1_000_000 * threadNumber) / numberTread;
+
+            Thread thread = new Thread(() -> {
+
+                    for (int i = 1; i <= 100_000; ++i) {
+                        int number = incrementWorkRange + i;
+                        String sqrtNumber = number + " - " + Math.sqrt(number)+ "\n";
+
+                        sqrtList[(number-1)]= sqrtNumber;
+                }
+
+            });
+            threads.add(thread);
+            thread.start();
+
+        }
+
+        for (Thread thread: threads) {
+            thread.join();
+        }
+
+        for (String thread: sqrtList) {
+            writerToFile.write(thread);
+            writerToFile.flush();
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("Took " + (end - startTime) + " ms.");
+    }
+}
